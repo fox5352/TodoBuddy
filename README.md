c8d3deb3b8d48811b67e6ec39d5bd733922721b4
christopher <voschristopher53@gmail.com>
2023-08-03 22:57:26 +0200

    added other project to footer links

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TodoBuddy
 
+warning <strong>create-react-app</strong> is no longer being maintained so I recommend you switch your projects to create-next-app this project still uses create-react-app
+
+install packages
+```
+npm i
+```
+then run
+```
+npm start
+```
+
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 import styles from "./css/Footer.module.css";
 
 export const Footer = () => {
-  const [trigger, setTrigger] = useState(false)
+  const [trigger, setTrigger] = useState(false);
 
-  useEffect(()=>{
-    const trigger = document.getElementsByClassName('trigger')[0].getBoundingClientRect().bottom
-    window.innerHeight > trigger? setTrigger(true) : setTrigger(false)
-  }, [trigger, setTrigger])
+  useEffect(() => {
+    const trigger = document.getElementsByClassName('trigger')[0].getBoundingClientRect().bottom;
+    window.innerHeight > trigger ? setTrigger(true) : setTrigger(false);
+  }, [trigger, setTrigger]);
 
-  window.addEventListener('scroll' ,()=>{
-    const trigger = document.getElementsByClassName('trigger')[0].getBoundingClientRect().bottom
-    window.innerHeight > trigger? setTrigger(true) : setTrigger(false)
-  })
+  window.addEventListener('scroll', () => {
+    const trigger = document.getElementsByClassName('trigger')[0].getBoundingClientRect().bottom;
+    window.innerHeight > trigger ? setTrigger(true) : setTrigger(false);
+  });
 
   return (
     <footer className={`trigger ${styles.trigger}`} >
-    <div className={`${styles.footer}  ${ trigger? styles.active: ''}`}>
-      <div className={styles.body}>
-        <h3>Contact Me</h3>
-        <div className={styles.block}>
-          <a className={styles.link} href='https://fox5352.github.io/online-cv'>online cv</a>
-          <a className={styles.link} href='https://www.linkedin.com/in/christopher-vos-6469b7284'>LinkedIn</a>
-        </div>
-        <h3>Projects</h3>
-        <div className={styles.block}>
-          <a className={styles.link} href='https://fox5352.github.io/simon-game'>Simon Game</a>
-          <a className={styles.link} href='https://52.201.225.92:3000'>Todo List</a>
+      <div className={`${styles.footer}  ${trigger ? styles.active : ''}`}>
+        <div className={styles.body}>
+          <h3>Contact Me</h3>
+          <div className={styles.block}>
+            <a className={styles.link} href='https://fox5352.github.io/online-cv'>online cv</a>
+            <a className={styles.link} href='https://www.linkedin.com/in/christopher-vos-6469b7284'>LinkedIn</a>
+          </div>
+          <h3>Projects</h3>
+          <div className={styles.block}>
+            <a className={styles.link} href='https://fox5352.github.io/simon-game'>Simon Game</a>
+            <a className={styles.link} href='https://ebooks-example.netlify.app/'>e-Books</a>
+          </div>
         </div>
       </div>
-    </div>
     </footer>
-  )
-}
+  );
+};
